358c0cb03824ca3042dbf8459227fe00aa2c1984
Begginning5 <140217500+Begginning5@users.noreply.github.com>
2025-09-18 15:44:42 +0000

    Your Message Here

--- Group2_Code.py
+++ Group2_Code.py
@@ -1,5 +1,13 @@
 #This is the running effort for the document.
 print("Hello Group2")
 print("Welcome To Wk1")
+
 #To run this code access terminal from the menu in GitHub.
+#To do this first click on "Code Spaces" in the Navigation Bar
+#Add you edits there.
 #In the GitHub Termanl type: Python3 Group2_Code.py
+#This is the difficult part:
+#to save your changes to GitHub you will need to use the following commands:
+#git add .
+#git commit -m "Your Message Here"
+#git push

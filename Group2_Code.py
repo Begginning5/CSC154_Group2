1d2e4b51dd3e0ca3f7d3e555bf3c84726c335285
Begginning5 <140217500+Begginning5@users.noreply.github.com>
2025-09-18 15:41:13 +0000

    I am Syncingronizing my Documents

---
+++ Group2_Code.py
@@ -0,0 +1,5 @@
+#This is the running effort for the document.
+print("Hello Group2")
+print("Welcome To Wk1")
+#To run this code access terminal from the menu in GitHub.
+#In the GitHub Termanl type: Python3 Group2_Code.py

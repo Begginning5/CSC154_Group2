bbcf112047fd4d39cf7c494ded9a0b5807195b8f
10,000 Crows in a Trench Coat <109446579+10kCrows@users.noreply.github.com>
2025-11-14 21:26:26 +0000

    Fixed Typo to Test Pushing Changes

--- Group2_Code.py
+++ Group2_Code.py
@@ -5,7 +5,7 @@
 # To run this code access terminal from the menu in GitHub.
 # To do this first click on "Code Spaces" in the Navigation Bar
 # Add you edits there.
-# In the GitHub Termanl type: python3 Group2_Code.py
+# In the GitHub Terminal type: python3 Group2_Code.py
 # This is the difficult part:
 # to save your changes to GitHub you will need to use the following commands:
 # git add .
